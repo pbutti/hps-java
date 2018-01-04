113755437cd3cfc4eb3935cf335f16aef8e5bc23
Sebouh Paul <sebouh.paul@gmail.com>
2018-01-04 15:17:54 -0500

    Made fixes to driver.  Also created steering file for running recon on early runs, before we started recording bias.  Ranges of event numbers where the bias was off are listed in the steering file, for the driver to filter out.  This is in place of using the bias flag for these runs.

--- recon/src/main/java/org/hps/recon/filtering/EventRangeFilter.java
+++ recon/src/main/java/org/hps/recon/filtering/EventRangeFilter.java
@@ -1,54 +1,46 @@
 package org.hps.recon.filtering;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
-
-//===> import org.hps.conditions.deprecated.SvtUtils;
-import org.lcsim.detector.identifier.IIdentifier;
-import org.lcsim.detector.tracker.silicon.ChargeCarrier;
-import org.lcsim.detector.tracker.silicon.HpsSiSensor;
-import org.lcsim.detector.tracker.silicon.SiSensor;
-import org.lcsim.detector.tracker.silicon.SiSensorElectrodes;
-import org.lcsim.detector.tracker.silicon.SiTrackerIdentifierHelper;
 import org.lcsim.event.EventHeader;
-import org.lcsim.event.RawTrackerHit;
 
 /**
  * 
  * @author Sebouh Paul
  */
 public class EventRangeFilter extends EventReconFilter{
-  
-    
+
+
     boolean inBadRange(int run, int event){
-       for(EventRange range : badRanges){
-           if(run == range.run && event >= range.min && event <= range.max)
-               return false;
-       }
-       return true;
+        for(EventRange range : badRanges){
+            if(run == range.run && event >= range.min && event <= range.max)
+                return false;
+        }
+        return true;
     }
-    
+
     @Override
     public void process(EventHeader event){
         incrementEventProcessed();
         if(inBadRange(event.getRunNumber(), event.getEventNumber()))
             skipEvent();
 
-        
+
         incrementEventPassed();
     }
-    
+
     ArrayList<EventRange> badRanges = new ArrayList();
 
-    
+
     public void setBadRanges(String s){
-        String split[] = s.split("[\n\r ,]+");
-        int n = split.length/3;
-        for(int i = 0; i< n; i++){
-            int run = Integer.parseInt(split[i*3]);
-            long min = Long.parseLong(split[i*3]+1);
-            long max = Long.parseLong(split[i*3]+2);
+        
+        Scanner scanner = new Scanner(s);
+        scanner.useDelimiter("[\n\r\t ,]+");
+        while (scanner.hasNextLong()){
+            int run = scanner.nextInt();
+            long min = scanner.nextLong();
+            long max = scanner.nextLong();
             badRanges.add(new EventRange(run, min, max));
         }
     }
