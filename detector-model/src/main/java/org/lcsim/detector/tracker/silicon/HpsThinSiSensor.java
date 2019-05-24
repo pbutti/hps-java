2da3bc54690d757efbce84bb09324158cd5dab8d
Matthew Solt <mrsolt@stanford.edu>
2019-05-24 14:56:08 -0700

    updating HpsThinSiSensor class to adjust electrode positions

--- detector-model/src/main/java/org/lcsim/detector/tracker/silicon/HpsThinSiSensor.java
+++ detector-model/src/main/java/org/lcsim/detector/tracker/silicon/HpsThinSiSensor.java
@@ -94,6 +94,7 @@ public class HpsThinSiSensor extends HpsSiSensor {
 
         System.out.println("Half Length Y " + sensorSolid.getYHalfLength());
         Hep3Vector electrodeShift = new BasicHep3Vector(0,-sensorSolid.getYHalfLength()/2,0);
+        //Hep3Vector electrodeShift = new BasicHep3Vector(0,0,0);
         // Translate to the outside of the sensor solid in order to setup the electrodes
         final ITranslation3D electrodesPosition1 = new Translation3D(VecOp.add(VecOp.mult(-pSide.getDistance(), pSide.getNormal()),electrodeShift));
         final ITranslation3D electrodesPosition2 = new Translation3D(VecOp.add(VecOp.mult(-pSide.getDistance(), pSide.getNormal()),VecOp.neg(electrodeShift)));
@@ -103,7 +104,7 @@ public class HpsThinSiSensor extends HpsSiSensor {
 
         // Align the strips with the edge of the sensor.
         final IRotation3D electrodesRotation1 = new RotationPassiveXYZ(0, 0, 0);
-        final IRotation3D electrodesRotation2 = new RotationPassiveXYZ(0, 0, Math.PI);
+        final IRotation3D electrodesRotation2 = new RotationPassiveXYZ(0, 0, 0);
         final Transform3D electrodesTransform1 = new Transform3D(electrodesPosition1, electrodesRotation1);
         final Transform3D electrodesTransform2 = new Transform3D(electrodesPosition2, electrodesRotation2);
         
@@ -113,36 +114,53 @@ public class HpsThinSiSensor extends HpsSiSensor {
         System.out.println("electrodesTransform2 " + electrodesTransform2);
 
         // Set the number of readout and sense electrodes.
-        final SiStripsThinSensor readoutElectrodes1 = new SiStripsThinSensor(ChargeCarrier.HOLE, getReadoutStripPitch(), this,electrodesTransform1);
-        final SiStripsThinSensor senseElectrodes1 = new SiStripsThinSensor(ChargeCarrier.HOLE, getSenseStripPitch(),this.getNumberOfSenseStrips(), this, electrodesTransform1);
-        final SiStripsThinSensor readoutElectrodes2 = new SiStripsThinSensor(ChargeCarrier.HOLE, getReadoutStripPitch(), this,electrodesTransform2);
-        final SiStripsThinSensor senseElectrodes2 = new SiStripsThinSensor(ChargeCarrier.HOLE, getSenseStripPitch(),this.getNumberOfSenseStrips(), this, electrodesTransform2);
+        final SiStrips readoutElectrodes1 = new SiStrips(ChargeCarrier.HOLE, getReadoutStripPitch(), this,electrodesTransform1);
+        final SiStrips senseElectrodes1 = new SiStrips(ChargeCarrier.HOLE, getSenseStripPitch(),this.getNumberOfSenseStrips(), this, electrodesTransform1);
+        final SiStrips readoutElectrodes2 = new SiStrips(ChargeCarrier.HOLE, getReadoutStripPitch(), this,electrodesTransform2);
+        final SiStrips senseElectrodes2 = new SiStrips(ChargeCarrier.HOLE, getSenseStripPitch(),this.getNumberOfSenseStrips(), this, electrodesTransform2);
 
-        System.out.println("Geometry " + readoutElectrodes1.getGeometry());
+        System.out.println("Geometry1 " + readoutElectrodes1.getGeometry());
         
-        List<Point3D> vertices = readoutElectrodes1.getGeometry().getVertices();
-        Point3D point1 = new Point3D(new BasicHep3Vector(vertices.get(0).x(),vertices.get(0).y()-sensorSolid.getYHalfLength()/2,vertices.get(0).z()));
-        Point3D point2 = new Point3D(new BasicHep3Vector(vertices.get(1).x(),vertices.get(1).y()-sensorSolid.getYHalfLength()/2,vertices.get(1).z()));
-        Point3D point3 = new Point3D(new BasicHep3Vector(vertices.get(2).x(),vertices.get(2).y()+sensorSolid.getYHalfLength()/2,vertices.get(2).z()));
-        Point3D point4 = new Point3D(new BasicHep3Vector(vertices.get(3).x(),vertices.get(3).y()+sensorSolid.getYHalfLength()/2,vertices.get(3).z()));
+        List<Point3D> vertices1 = readoutElectrodes1.getGeometry().getVertices();
+        Point3D point1_1 = new Point3D(new BasicHep3Vector(vertices1.get(0).x(),vertices1.get(0).y()-sensorSolid.getYHalfLength()/2,vertices1.get(0).z()));
+        Point3D point2_1 = new Point3D(new BasicHep3Vector(vertices1.get(1).x(),vertices1.get(1).y()-sensorSolid.getYHalfLength()/2,vertices1.get(1).z()));
+        Point3D point3_1 = new Point3D(new BasicHep3Vector(vertices1.get(2).x(),vertices1.get(2).y()+sensorSolid.getYHalfLength()/2,vertices1.get(2).z()));
+        Point3D point4_1 = new Point3D(new BasicHep3Vector(vertices1.get(3).x(),vertices1.get(3).y()+sensorSolid.getYHalfLength()/2,vertices1.get(3).z()));
         //System.out.println("Point1 " + point1);
         //System.out.println("Point2 " + point2);
         //System.out.println("Point3 " + point3);
         //System.out.println("Point4 " + point4);
-        List<Point3D> newvertices = new ArrayList<>();
-        newvertices.add(point1);
-        newvertices.add(point2);
-        newvertices.add(point3);
-        newvertices.add(point4);
+        List<Point3D> newvertices1 = new ArrayList<>();
+        newvertices1.add(point1_1);
+        newvertices1.add(point2_1);
+        newvertices1.add(point3_1);
+        newvertices1.add(point4_1);
         
-        readoutElectrodes1.setGeometry(new Polygon3D(newvertices));
-        senseElectrodes1.setGeometry(new Polygon3D(newvertices));
-        readoutElectrodes2.setGeometry(new Polygon3D(newvertices));
-        senseElectrodes2.setGeometry(new Polygon3D(newvertices));
+        System.out.println("Geometry2 " + readoutElectrodes2.getGeometry());
+        
+        List<Point3D> vertices2 = readoutElectrodes2.getGeometry().getVertices();
+        Point3D point1_2 = new Point3D(new BasicHep3Vector(vertices2.get(0).x(),vertices2.get(0).y()-sensorSolid.getYHalfLength()/2,vertices2.get(0).z()));
+        Point3D point2_2 = new Point3D(new BasicHep3Vector(vertices2.get(1).x(),vertices2.get(1).y()-sensorSolid.getYHalfLength()/2,vertices2.get(1).z()));
+        Point3D point3_2 = new Point3D(new BasicHep3Vector(vertices2.get(2).x(),vertices2.get(2).y()+sensorSolid.getYHalfLength()/2,vertices2.get(2).z()));
+        Point3D point4_2 = new Point3D(new BasicHep3Vector(vertices2.get(3).x(),vertices2.get(3).y()+sensorSolid.getYHalfLength()/2,vertices2.get(3).z()));
+        //System.out.println("Point1 " + point1);
+        //System.out.println("Point2 " + point2);
+        //System.out.println("Point3 " + point3);
+        //System.out.println("Point4 " + point4);
+        List<Point3D> newvertices2 = new ArrayList<>();
+        newvertices2.add(point1_2);
+        newvertices2.add(point2_2);
+        newvertices2.add(point3_2);
+        newvertices2.add(point4_2);
+        
+        readoutElectrodes1.setGeometry(new Polygon3D(newvertices1));
+        senseElectrodes1.setGeometry(new Polygon3D(newvertices1));
+        readoutElectrodes2.setGeometry(new Polygon3D(newvertices2));
+        senseElectrodes2.setGeometry(new Polygon3D(newvertices2));
         
         System.out.println("Geometry Redo Readout 1" + readoutElectrodes1.getGeometry());
-        System.out.println("Geometry Redo Sense 2" + senseElectrodes1.getGeometry());
-        System.out.println("Geometry Redo Readout 1" + readoutElectrodes2.getGeometry());
+        System.out.println("Geometry Redo Sense 1" + senseElectrodes1.getGeometry());
+        System.out.println("Geometry Redo Readout 2" + readoutElectrodes2.getGeometry());
         System.out.println("Geometry Redo Sense 2" + senseElectrodes2.getGeometry());
 
         final double readoutCapacitance = this.getStripLength() > this.longSensorLengthThreshold ? this.readoutLongStripCapacitanceSlope
@@ -165,6 +183,10 @@ public class HpsThinSiSensor extends HpsSiSensor {
         this.setReadoutElectrodes(readoutElectrodes1);
         this.setSenseElectrodes(senseElectrodes2);
         this.setReadoutElectrodes(readoutElectrodes2);
+        
+        System.out.println("Sense Electrodes 1 " + senseElectrodes1 + "  readoutElectrodes 1 " + readoutElectrodes1);
+        System.out.println("Sense Electrodes 2 " + senseElectrodes2 + "  readoutElectrodes 2 " + readoutElectrodes2);
+        System.out.println("Sense Electrodes " + this.getSenseElectrodes() + "  readoutElectrodes " + this.getReadoutElectrodes());
 
         // Set the charge transfer efficiency of both the sense and readout
         // strips.
